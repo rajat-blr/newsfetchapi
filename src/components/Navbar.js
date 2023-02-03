ca4f22204976b57f93d989d6a28a5b1be0fd35b2
rajat <vrajatlink@gmail.com>
2023-02-03 18:01:15 +0530

    Added fully working Dark Mode to the UI

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types'
 export default function navbar(props) {
   return (
     <div>
-          <nav className="navbar navbar-expand-lg bg-body-tertiary">
+          <nav className={`navbar navbar-expand-lg navbar-${props.mode} bg-${props.mode}`}>
   <div className="container-fluid">
     <a className="navbar-brand" href="/">{props.title}</a>
     <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
@@ -23,6 +23,10 @@ export default function navbar(props) {
         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
         <button className="btn btn-outline-success" type="submit">Search</button>
       </form>
+      <div className={`form-check form-switch text-${props.mode==='light'?'dark':'light'} mx-3`}>
+  <input className="form-check-input" onClick={props.toggleMode} type="checkbox" role="switch" id="flexSwitchCheckDefault"/>
+  <label className="form-check-label" htmlFor="flexSwitchCheckDefault">Enable Dark Mode</label>
+</div>
     </div>
   </div>
 </nav>
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -18,17 +18,17 @@ export default function TextForm(props) {
   const[text,setText] = useState('Enter text here2');
   return (
 <>
-  <div>
+  <div className="container" style={{color: props.mode ==='dark'?'white':'black'}}>
     <h1>{props.heading}</h1>
    <div className="mb-3">
     <label htmlFor="myBox" className="form-label">Example textarea</label>
-    <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="10"></textarea>
+    <textarea className="form-control" style={{backgroundColor: props.mode ==='dark'?'grey':'white',color:props.mode ==='dark'?'white':'black'}} value={text} onChange={handleOnChange} id="myBox" rows="10"></textarea>
    </div>
    <button className="btn btn-primary" onClick={handleUpClick}>Convert to UpperCase</button>
    <button className="btn btn-primary mx-3" onClick={handleLowClick}>Convert to LowerCase</button>
    
   </div>
-  <div class="container my-3">
+  <div className="container my-3" style={{color: props.mode ==='dark'?'white':'black'}}>
     <h1>Your text summary</h1>
     <p>{text.split(" ").length} words and {text.length} characters</p>
     <p>{0.008 * text.split(" ").length} minutes read</p>
