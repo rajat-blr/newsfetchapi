f9e943ca0bce549ff9aa16d6127b4612d98e324b
rajat <vrajatlink@gmail.com>
2023-02-10 11:06:12 +0530

    Added auto dismissing alert to dark mode/light mode

--- src/App.js
+++ src/App.js
@@ -4,26 +4,37 @@ import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import React, {useState} from 'react'
+import Alert from './components/Alert';
 
 
 
 
 function App() { 
   const [mode, setMode] = useState('light');
+  const [alert, setalert] = useState(null);
 
+  const showAlert = (message,type)=>{
+    setalert({
+      msg: message,
+      type: type
+    })
+  }
   const toggleMode =()=>{
     if(mode==='light'){
       setMode('dark');
       document.body.style.backgroundColor='grey';
+      showAlert("Dark mode has been enabled","success")
     }
     else{
       setMode('light');
       document.body.style.backgroundColor='white';
+      showAlert("Light mode has been enabled","success")
     }
   }
   return (
     <>
     <Navbar title="TextUtils" aboutText="About" mode={mode}  toggleMode={toggleMode}/>
+    <Alert alert={alert}/>
     <div className="container my-3">
     <TextForm heading="Enter the text to analyze" mode={mode}/>
     {/* <About/> */}
